4a6d0df5a3fbaabdf398aeb5e13db25a555c76d4
abdlmnf17 <11210195@bsi.ac.id>
2024-06-22 08:13:43 +0700

    Rilis Aplikasi Perpustakaan Sekolah

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,3 @@
-
-
 <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
     <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">SUKSES</span>
     <span class="font-semibold mr-2 text-left flex-auto">{{ $message }}</span>
