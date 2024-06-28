e15a8e46edd952f32972540ad41e1f14f9ae3d62
abdlmnf17 <11210195@bsi.ac.id>
2024-06-28 18:52:42 +0700

    [UPDATE] Memperbarui layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/assets/app.css">
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
@@ -41,4 +40,6 @@
 
     @include('layouts.footer')
 </body>
+
+<script src="/assets/app.js"></script>
 </html>
